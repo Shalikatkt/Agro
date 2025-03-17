675778856c7ece1f01332db7de046bcb5f1c9215
Shalikatkt <162815099+Shalikatkt@users.noreply.github.com>
2025-03-17 22:31:20 +0530

    add footer

--- agro/src/app/page.tsx
+++ agro/src/app/page.tsx
@@ -2,12 +2,18 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import NavBar from '../components/NavBar';
+import Home from '../components/Home';
+import Footer from '../components/Footer';
+import Testino from '../components/Testino';
 
-export default function Home() {
+export default function Hme() {
 
   return (
     <div>
       <NavBar/>
+      <Home/>
+      <Testino/>
+      <Footer/>
     </div>
   );
 }
